8b333e9ac5674449c30f49dabbdba5c68b861785
Jasper2002 <78854014+Woofium@users.noreply.github.com>
2022-04-07 10:09:00 +0200

    Initial Commit

---
+++ app/controllers/Countries.php
@@ -0,0 +1,38 @@
+<?php
+class Countries extends Controller {
+
+  public function __construct() {
+    $this->countryModel = $this->model('Country');
+  }
+
+  public function index() {
+    /**
+     * Haal via de method getFruits() uit de model Fruit de records op
+     * uit de database
+     */
+    $countries = $this->countryModel->getCountries();
+
+    /**
+     * Maak de inhoud voor de tbody in de view
+     */
+    $rows = '';
+    foreach ($countries as $value){
+      $rows .= "<tr>
+                  <td>$value->id</td>
+                  <td>" . htmlentities($value->name, ENT_QUOTES, 'ISO-8859-1') . "</td>
+                  <td>" . htmlentities($value->capitalCity, ENT_QUOTES, 'ISO-8859-1') . "</td>
+                  <td>" . htmlentities($value->continent, ENT_QUOTES, 'ISO-8859-1') . "</td>
+                  <td>" . number_format($value->population, 0, ',', '.') . "</td>
+                </tr>";
+    }
+
+
+    $data = [
+      'title' => '<h1>Landenoverzicht</h1>',
+      'countries' => $rows
+    ];
+    $this->view('countries/index', $data);
+  }
+}
+
+?>
